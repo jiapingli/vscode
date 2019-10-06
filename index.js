fb0d619c70123a0f23b8ebae6017ecfd4de0d8cc
jiapingli <1771500175@qq.com>
2019-10-06 20:00:58 +0800

    提交修改

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
-let a = 1;
-const a=1;
-var a = 1;
+function f() {
+    try{
+        console.log("1s后执行中");
+        throw new Error("错误信息");
+    } catch (err) {
+        console.log(err)
+    }
+  
+  
+}
+
+try {
+  setTimeout(f, 1000);
+} catch (err) {
+  console.log(err);
+} finally {
+  console.log("执行了吗？");
+}
